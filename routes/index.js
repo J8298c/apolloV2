0ffc7752f365069f6be3bdc3555657fbb53b268e
j8298c <juliocmojica@gmail.com>
2017-05-23 22:42:57 -0400

    added test workout and workout page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,43 @@ router.get('/all', (req, res, next)=>{
   console.log(name);
 })
 
+router.get('/workout/:name', (req, res, next)=>{
+  res.render('workout', 
+  {name: req.params.name,
+    workout:[{
+      name: 'jog',
+      set: '10 mins',
+      reps: 1
+    },{
+      name: 'pullups',
+      set: 3,
+      reps: 3
+    },{
+      name: 'pushups',
+      set: 3,
+      reps: 10
+    },{
+      name: 'squats',
+      set: 3,
+      reps: 10
+    },{
+      name: 'lat pull-down',
+      set: 5,
+      reps: 10
+    },{
+      name: 'dumbbell-rows',
+      set: 5,
+      reps: 10
+    },{
+      name: 'barbell-curls',
+      set: 5,
+      reps: 10
+    },{
+      name: 'curls',
+      set: 5,
+      reps: 10
+    }]
+  })
+});
+
 module.exports = router;
