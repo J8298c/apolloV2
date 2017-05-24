bbf8c1bdde1de829ed7cd1513d153083985758b6
j8298c <juliocmojica@gmail.com>
2017-05-24 09:04:00 -0400

    adding signup page mdl for styling

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/*
+    User Login 
+*/
 router.get('/login', (req, res, next)=> {
   res.render('login');
 });
+
+router.post('/login', (req, res, next)=>{
+  //put logic in here
+})
+
+
+/*
+    User-signup
+*/
+
+router.get('/signup', (req, res, next)=>{
+  res.render('signup');
+})
+
+router.post('/signup', (req, res, next)=>{
+  console.log(req.body);
+  //put mongoose logic here
+})
 
 module.exports = router;
